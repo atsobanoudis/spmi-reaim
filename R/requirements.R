40bb551139a2144c07ff0f586f111b2cd845d1a2
Alex Tsobanoudis <alex.tsobanoudis@kp.org>
2025-11-30 14:42:19 -0800

    basic reqs

---
+++ R/requirements.R
@@ -0,0 +1,19 @@
+# R package requirements for this project
+
+required_packages <- c(
+  "readxl",
+  "dplyr",
+  "tidyr",
+  "stringr",
+  "readr"
+)
+
+install_if_missing <- function(pkgs) {
+  missing <- pkgs[!(pkgs %in% rownames(installed.packages()))]
+  if (length(missing)) {
+    install.packages(missing, dependencies = TRUE)
+  }
+}
+
+install_if_missing(required_packages)
+invisible(lapply(required_packages, library, character.only = TRUE))
